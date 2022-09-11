cc6b37bd06b049a8779cdb14f3320900b31e247c
guxukai <gxk@33.cn>
2022-09-11 14:21:58 +0800

    deployer(gh-pages): deploy to github pages

---
+++ packages/lianliankan/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+#npm run build
+
+# navigate into the build output directory
+#cd dist/
+
+# if you are deploying to a custom domain
+#echo 'iro.nyan.my.id' > CNAME
+
+#cd -
+
+#git add dist/ -f
+git add .
+git commit -m 'deployer(gh-pages): deploy to github pages'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:laughing-team/planet.git main:gh-pages
+
+git push  -f git@github.com:gxkai/gxkai.github.io.git main
